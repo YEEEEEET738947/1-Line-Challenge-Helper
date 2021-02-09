110044494831497ebf26e86758cdd38cb583f2e4
YEEEEEET738947 <69402664+YEEEEEET738947@users.noreply.github.com>
2021-02-09 11:44:50 +0000

    Update Scratch-Minion-Helper.py

--- Scratch-Minion-Helper.py
+++ Scratch-Minion-Helper.py
@@ -25,6 +25,7 @@ try:
     bee = bee.convert('HSV')
     pixels = bee.load()
     for x in range(bee.size[0]):
+        
         for y in range(bee.size[1]):
             c += str(round((pixels[x, 359-y][0] / 255) * 100)).zfill(3)
             s += str(round((pixels[x, 359-y][1] / 255) * 100)).zfill(3)
@@ -38,6 +39,6 @@ try:
 
     with open("b.txt", "w+") as f:
         f.write(b)
-    print("Remix my scratch project and copy and paste c.txt to the c variable, b.txt to the b variable and s.txt to the s variable. Don't forget to thank me")
+    print("Remix my scratch project and copy and paste c.txt to the c variable, b.txt to the b variable and s.txt to the s variable. Don't forget to thank me @goomaster242")
 except:
     print("file not in folder, does not exist or you spelt it wrong")
